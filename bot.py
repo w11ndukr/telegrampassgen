e514c2a90f84f0c8af89046015e18ff55c677f55
w11ndukr <129502439+w11ndukr@users.noreply.github.com>
2023-04-02 16:31:14 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import random
 import string
-from click import Context
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
@@ -12,49 +11,82 @@ def generate_password(length: int, special_chars: bool = False, uppercase: bool 
         chars += string.ascii_uppercase
     return ''.join(random.choice(chars) for _ in range(length))
 
-def start(update: Update, _: CallbackContext) -> None:
-    update.message.reply_text('Привет! Я бот-генератор паролей. Напиши /help, чтобы узнать, что я могу.')
+def start(update: Update, context: CallbackContext) -> None:
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Привет! Я бот-генератор паролей. Напиши /help, чтобы узнать, что я могу.')
 
-def help(update: Update, _: CallbackContext) -> None:
-    update.message.reply_text('/password - сгенерировать новый пароль\n/save - сохранить пароль к определенному сервису')
+def help(update: Update, context: CallbackContext) -> None:
+    context.bot.send_message(chat_id=update.effective_chat.id, text='/password - сгенерировать новый пароль\n/save - сохранить пароль к определенному сервису')
 
-def password(update: Update, _: CallbackContext) -> None:
-    update.message.reply_text('Выбери длину пароля (от 4 до 28):')
-    length = int(update.message.text)
-    update.message.reply_text('Включить специальные символы (да/нет)?')
-    special_chars = update.message.text.lower() == 'да'
-    update.message.reply_text('Включить заглавные буквы (да/нет)?')
-    uppercase = update.message.text.lower() == 'да'
-    password = generate_password(length, special_chars, uppercase)
-    update.message.reply_text(f'Твой новый пароль: {password}')
+def password(update: Update, context: CallbackContext) -> None:
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Выбери длину пароля (от 4 до 28):')
+    context.user_data['password_length'] = True
 
-def save(update: Update, _: CallbackContext) -> None:
-    update.message.reply_text('Введи название сервиса:')
-    service = update.message.text
-    update.message.reply_text('Введи мастер-пароль для доступа к сохраненным паролям:')
-    master_password = update.message.text
-    update.message.reply_text('Выбери длину пароля (от 4 до 28):')
-    length = int(update.message.text)
-    update.message.reply_text('Включить специальные символы (да/нет)?')
-    special_chars = update.message.text.lower() == 'да'
-    update.message.reply_text('Включить заглавные буквы (да/нет)?')
-    uppercase = update.message.text.lower() == 'да'
-    password = generate_password(length, special_chars, uppercase)
-    with open('passwords.txt', 'a') as f:
-        f.write(f'{service}:{password}\n')
-    update.message.reply_text(f'Пароль для сервиса {service} успешно сохранен.')
+def save(update: Update, context: CallbackContext) -> None:
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Введи название сервиса:')
+    context.user_data['service'] = True
 
-TOKEN = '5969795803:AAFwj401KLrSwX34s8QqUrjXpb9kl-cWF10'
+def validate_password_length(update: Update, context: CallbackContext) -> None:
+    try:
+        length = int(update.message.text)
+        if length < 4 or length > 28:
+            raise ValueError('Длина пароля должна быть от 4 до 28 символов.')
+        context.user_data['password_length'] = length
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Включить специальные символы (да/нет)?')
+        context.user_data['special_chars'] = True
+    except ValueError as e:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=str(e))
+
+def validate_special_chars(update: Update, context: CallbackContext) -> None:
+    try:
+        special_chars = update.message.text.lower() == 'да'
+        context.user_data['special_chars'] = special_chars
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Включить заглавные буквы (да/нет)?')
+        context.user_data['uppercase'] = True
+    except ValueError as e:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=str(e))
+
+def validate_uppercase(update: Update, context: CallbackContext) -> None:
+    try:
+        uppercase = update.message.text.lower() == 'да'
+        context.user_data['uppercase'] = uppercase
+        password = generate_password(context.user_data['password_length'], context.user_data['special_chars'], context.user_data['uppercase'])
+        context.bot.send_message(chat_id=        update.effective_chat.id, text=f'Ваш пароль: {password}')
+        context.user_data.clear()
+    except ValueError as e:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=str(e))
+
+def save_password(update: Update, context: CallbackContext) -> None:
+    try:
+        service = update.message.text
+        password = generate_password(context.user_data['password_length'], context.user_data['special_chars'], context.user_data['uppercase'])
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f'Пароль для сервиса {service}: {password}')
+        context.user_data.clear()
+    except KeyError:
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Начните генерацию пароля с команды /password')
 
 def main() -> None:
-    updater = Updater(token=TOKEN, context=Context())
+    updater = Updater(token=TOKEN, context=Context(command=Command))
     dispatcher = updater.dispatcher
+
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('help', help))
     dispatcher.add_handler(CommandHandler('password', password))
     dispatcher.add_handler(CommandHandler('save', save))
+
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command & ~Filters.regex('^/'), 
+                                           validate_password_length))
+    dispatcher.add_handler(MessageHandler(Filters.regex('^(да|нет)$') & Filters.update.message & 
+                                           Filters.user(user_id=update.effective_user.id),
+                                           validate_special_chars))
+    dispatcher.add_handler(MessageHandler(Filters.regex('^(да|нет)$') & Filters.update.message & 
+                                           Filters.user(user_id=update.effective_user.id),
+                                           validate_uppercase))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command & ~Filters.regex('^/'),
+                                           save_password))
+
     updater.start_polling()
     updater.idle()
 
 if __name__ == '__main__':
     main()
+
